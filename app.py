f6018df3114147e132c9e42f3ac7f4af430d921e
hanami001 <natthakorn.rue@gmail.com>
2025-01-26 08:22:19 +0000

    t6

--- app.py
+++ app.py
@@ -13,13 +13,17 @@ import random
 st.set_page_config(layout="wide")
 st.title('Test Streamlit')
 
-con1 = st.container()
-for col in con1.columns([1, 2, 3, 4], border=True):
-    col.write("Hello World!")
+st.write('Hello World!')
 
-con2 = st.container(height=100)
-cc2_1, cc2_2, cc2_3, cc2_4 = con2.columns(4)
-cc2_1.write("Column 5 @ Container 2")
-cc2_2.write("Column 6 @ Container 2")
-cc2_3.write("Column 7 @ Container 2")
-cc2_4.write("Column 8 @ Container 2")
+st.sidebar.title('Filter')
+st.sidebar.header('Option')
+st.sidebar.selectbox(
+    "Pls Select One!",
+    ('opt1', 'opt2', 'opt3')
+)
+
+with st.sidebar:
+    st.radio(
+        "Pls Choose One!",
+        ['cho1', 'cho2', 'cho3'],
+    )
